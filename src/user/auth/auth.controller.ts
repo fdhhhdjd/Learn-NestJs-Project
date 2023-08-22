6004c5ce6651be2a5a32488442ef6ad6291b8a07
Nguyễn Tiến Tài <63393170+fdhhhdjd@users.noreply.github.com>
2023-08-22 16:50:33 +0700

    #11 feature:Signin (#13)

--- src/user/auth/auth.controller.ts
+++ src/user/auth/auth.controller.ts
@@ -5,13 +5,19 @@ import { Controller, Post, Body } from '@nestjs/common';
 import { AuthService } from './auth.service';
 
 //* DTOS
-import { SignupDto } from 'src/dtos/auth.dto';
+import { SigninDto, SignupDto } from 'src/dtos/auth.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+
   @Post('signup')
   signup(@Body() body: SignupDto) {
     return this.authService.signup(body);
+  }
+
+  @Post('signin')
+  signin(@Body() body: SigninDto) {
+    return this.authService.signin(body);
   }
 }
